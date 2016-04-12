ea072aacf6e99c915b5cb2e9668294de7f13aca6
Vitaly Lobchuk <vn.lobchuk@gmail.com>
2016-04-12 11:09:03 +0300

    fix event

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -4,6 +4,9 @@ namespace Isolate\ConsoleServiceProvider\Console;
 
 use Symfony\Component\Console\Application as BaseApplication;
 use Pimple\Container;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 class Application extends BaseApplication
 {
@@ -16,22 +19,35 @@ class Application extends BaseApplication
    * @var string
    */
   private $basePath;
+  /**
+   * @var EventDispatcherInterface
+   */
+  private $dispatcher;
 
   /**
    * @param Container $container
    * @param string $basePath
    * @param string $name
    * @param string $version
+   * @param EventDispatcherInterface $dispatcher
    */
-  public function __construct(Container $container, $basePath, $name = 'UNKNOWN', $version = 'UNKNOWN')
+  public function __construct(Container $container, $basePath, $name = 'UNKNOWN', $version = 'UNKNOWN', EventDispatcherInterface $dispatcher)
   {
     parent::__construct($name, $version);
 
     $this->container = $container;
     $this->basePath = $basePath;
+    $this->dispatcher = $dispatcher;
 
     $container->boot();
   }
+
+  public function run(InputInterface $input = null, OutputInterface $output = null)
+  {
+    $this->dispatcher->dispatch(ConsoleEvents::INIT, new ConsoleEvent($this));
+    return parent::run($input, $output);
+  }
+
 
   /**
    * @return Container
--- src/ConsoleServiceProvider.php
+++ src/ConsoleServiceProvider.php
@@ -4,8 +4,6 @@ namespace Isolate\ConsoleServiceProvider;
 
 use Pimple\Container;
 use Pimple\ServiceProviderInterface;
-use Isolate\ConsoleServiceProvider\Console\ConsoleEvent;
-use Isolate\ConsoleServiceProvider\Console\ConsoleEvents;
 use Isolate\ConsoleServiceProvider\Console\Application as ConsoleApplication;
 
 class ConsoleServiceProvider implements ServiceProviderInterface
@@ -15,31 +13,19 @@ class ConsoleServiceProvider implements ServiceProviderInterface
    *
    * This method should only be used to configure services and parameters.
    * It should not get services.
+   * @param Container $container
    */
   public function register(Container $container)
   {
-    $container['console'] = function() use ($container) {
+    $container['console'] = function () use ($container) {
       $console = new ConsoleApplication(
         $container,
         $container['console.base_path'],
         $container['console.name'],
-        $container['console.version']
+        $container['console.version'],
+        $container['dispatcher']
       );
-
-      $container['dispatcher']->dispatch(ConsoleEvents::INIT, new ConsoleEvent($console));
-
       return $console;
     };
-  }
-
-  /**
-   * Bootstraps the application.
-   *
-   * This method is called after all services are registered
-   * and should be used for "dynamic" configuration (whenever
-   * a service must be requested).
-   */
-  public function boot(Application $app)
-  {
   }
 }
